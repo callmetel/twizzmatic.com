d8bd5b8e787efdb78814437f598fa973fbfa3f7f
callmetel <info@callmetel.com>
2021-05-22 00:45:22 -0400

    add prefooter

--- wp-content/themes/twizzmatic/src/footer.php
+++ wp-content/themes/twizzmatic/src/footer.php
@@ -1,3 +1,13 @@
+      <section id="pre-footer">
+      	<div class="row">
+      		<div class="col">
+      			<h4 class="prefooter-title">Stay In <span>Touch</span></h4>
+      		</div>
+      		<div class="col">
+      			<?php echo do_shortcode('[yikes-mailchimp form="1"]'); ?>
+      		</div>
+      	</div>
+      </section>
       <footer class="footer">
         <p class="footer__copyright copyright">
           &copy; <?php echo date('Y'); ?> <?php _e('Copyright', 'wp-blank') ?> <?php bloginfo('name'); ?>
