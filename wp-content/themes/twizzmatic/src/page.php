2b95bf17fe62c5d85bddc5bba7ebd53dc847185f
callmetel <info@callmetel.com>
2021-05-22 08:57:15 -0400

    add partial home

--- wp-content/themes/twizzmatic/src/page.php
+++ wp-content/themes/twizzmatic/src/page.php
@@ -1,20 +1,28 @@
 <?php get_header(); ?>
 
   <main>
-    <section>
-      <h1><?php the_title(); ?></h1>
-      <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <?php the_content(); ?>
-          <?php comments_template('', true); ?>
-        </article>
-      <?php endwhile; ?>
-      <?php else: ?>
-        <article>
-          <h2><?php _e('Sorry, nothing to display.', 'wp-blank'); ?></h2>
-        </article>
-      <?php endif; ?>
-    </section>
+    <?php
+      if(is_front_page()):
+        get_template_part( 'partials/home' );
+      else:
+    ?>
+      <section>
+        <h1><?php the_title(); ?></h1>
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php the_content(); ?>
+            <?php comments_template('', true); ?>
+          </article>
+        <?php endwhile; ?>
+        <?php else: ?>
+          <article>
+            <h2><?php _e('Sorry, nothing to display.', 'wp-blank'); ?></h2>
+          </article>
+        <?php endif; ?>
+      </section>
+    <?php
+      endif;
+    ?>
   </main>
 
 <?php get_sidebar(); ?>
