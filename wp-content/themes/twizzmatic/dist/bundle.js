a3028ebdd9cae82e963249ec93cd8bfe6ae7fb05
callmetel <info@callmetel.com>
2021-05-22 19:15:45 -0400

    update base styles

--- wp-content/themes/twizzmatic/dist/bundle.js
+++ wp-content/themes/twizzmatic/dist/bundle.js
@@ -108,6 +108,16 @@
       e.preventDefault();
       $('body').toggleClass('nav-open');
     });
+
+    /*********************
+    ** Main Body Functions
+    **********************/
+
+    $(window).resize(function () {
+      var $headerHeight = $('#main-header').outerHeight();
+
+      $('main').css('padding-top', $headerHeight + 10);
+    }).resize();
   });
 })(jQuery);
 
--- wp-content/themes/twizzmatic/src/js/main.js
+++ wp-content/themes/twizzmatic/src/js/main.js
@@ -11,5 +11,15 @@
     		$('body').toggleClass('nav-open');
     	});
 
+    /*********************
+    ** Main Body Functions
+    **********************/
+
+    	$(window).resize(function(){
+    		var $headerHeight = $('#main-header').outerHeight();
+
+    		$('main').css('padding-top', $headerHeight+10);
+    	}).resize();
+
   });
 }(jQuery));
