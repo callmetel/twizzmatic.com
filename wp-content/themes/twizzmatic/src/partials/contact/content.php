bf4f90af249f63febbeccfb55983484b21b4c52c
callmetel <info@callmetel.com>
2021-05-23 09:52:20 -0400

    update contact page elements

--- wp-content/themes/twizzmatic/src/partials/contact/content.php
+++ wp-content/themes/twizzmatic/src/partials/contact/content.php
@@ -1,13 +1,25 @@
 <?php 
 	// Get ACF Fields
 	
-	$title = the_field('title');
+	$title = get_field('title');
+	$email = get_field('email');
 ?>
 
 <section id="contact-banner">
 	<div class="row">
 		<div class="col">
 			<h1 class="contact-title title--large"><?php echo $title; ?></h1>
+		</div>
+	</div>
+</section>
+
+<section id="contact-info">
+	<div class="row">
+		<div class="col">
+			<a href="mailto:<?php echo $email; ?>" class="contact-email">
+				<span class="email-title">Email Me</span>
+				<span class="email-address"><?php echo $email; ?></span>
+			</a>
 		</div>
 	</div>
 </section>
