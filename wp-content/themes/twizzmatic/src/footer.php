5c383c40dd58bbf44b5d440255735ff40a657010
callmetel <info@callmetel.com>
2021-05-22 20:10:19 -0400

    update footer

--- wp-content/themes/twizzmatic/src/footer.php
+++ wp-content/themes/twizzmatic/src/footer.php
@@ -1,17 +1,19 @@
-      <section id="pre-footer">
-      	<div class="row">
-      		<div class="col">
-      			<h4 class="prefooter-title">Stay In <span>Touch</span></h4>
-      		</div>
-      		<div class="col">
-      			<?php echo do_shortcode('[yikes-mailchimp form="1"]'); ?>
-      		</div>
-      	</div>
-      </section>
-      <footer class="footer">
-        <p class="footer__copyright copyright">
-          &copy; <?php echo date('Y'); ?> <?php _e('Copyright', 'wp-blank') ?> <?php bloginfo('name'); ?>
-        </p>
+      <footer id="main-footer">
+        <div class="prefooter">
+          <div class="row grid grid-2">
+            <div class="col">
+              <h4 class="prefooter-title">Stay In <span>Touch</span></h4>
+            </div>
+            <div class="col">
+              <?php echo do_shortcode('[yikes-mailchimp form="1"]'); ?>
+            </div>
+          </div>
+        </div>
+        <div class="footer">
+          <div class="row">
+            
+          </div>
+        </div>
       </footer>
     </div>
     <?php wp_footer(); ?>
