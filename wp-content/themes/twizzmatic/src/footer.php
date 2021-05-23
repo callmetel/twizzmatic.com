0dc539cf2369ea47df852cd9432e2eb9fdbb737a
callmetel <info@callmetel.com>
2021-05-22 22:13:39 -0400

    update footer

--- wp-content/themes/twizzmatic/src/footer.php
+++ wp-content/themes/twizzmatic/src/footer.php
@@ -3,6 +3,20 @@
           <div class="row grid grid-2">
             <div class="col">
               <h4 class="prefooter-title">Stay In <span>Touch</span></h4>
+              <div class="social-icons">
+                <a href="https://instagram.com" target="_blank" class="icon">
+                  <img src="<?php echo get_template_directory_uri() . '/src/assets/images/icon-instagram.svg'; ?>" width="30" alt="TwizzMatic Instagram">
+                </a>
+                <a href="https://facebook.com" target="_blank" class="icon">
+                  <img src="<?php echo get_template_directory_uri() . '/asset/src/assets/images/icon-facebook.svg'; ?>" width="30" alt="TwizzMatic Facebook">
+                </a>
+                <a href="https://twitter.com" target="_blank" class="icon">
+                  <img src="<?php echo get_template_directory_uri() . '/asset/src/assets/images/icon-twitter.svg'; ?>" width="30" alt="TwizzMatic Twitter">
+                </a>
+                <a href="https://youtube.com" target="_blank" class="icon">
+                  <img src="<?php echo get_template_directory_uri() . '/asset/src/assets/images/icon-youtube.svg'; ?>" width="30" alt="TwizzMatic Youtube">
+                </a>
+              </div>
             </div>
             <div class="col">
               <?php echo do_shortcode('[yikes-mailchimp form="1"]'); ?>
