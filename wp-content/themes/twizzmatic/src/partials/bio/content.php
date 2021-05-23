8edb0a1419af48ee8985df6ea1b64fa152520f21
callmetel <info@callmetel.com>
2021-05-23 11:21:51 -0400

    add bio page styling

--- wp-content/themes/twizzmatic/src/partials/bio/content.php
+++ wp-content/themes/twizzmatic/src/partials/bio/content.php
@@ -9,7 +9,7 @@
 
 <section id="bio-content">
 	<div class="row">
-		<div class="col">
+		<div class="col col-1">
 			<div class="bio-image">
 				<img src="<?php echo $image; ?>" alt="TwizzMatic">
 			</div>
@@ -17,7 +17,7 @@
 				<?php echo $quote; ?>
 			</div>
 		</div>
-		<div class="col">
+		<div class="col col-2">
 			<h1 class="bio-title yellow"><?php echo $title; ?></h1>
 			<div class="full-bio">
 				<div class="inner">
