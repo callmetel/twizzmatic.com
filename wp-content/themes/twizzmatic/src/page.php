84a199bb3daf58fef72ec4cefdcb068e0fdad8e8
callmetel <info@callmetel.com>
2021-05-23 11:04:08 -0400

    add bio page elements

--- wp-content/themes/twizzmatic/src/page.php
+++ wp-content/themes/twizzmatic/src/page.php
@@ -4,23 +4,33 @@
     <?php
       if(is_front_page()):
         get_template_part( 'partials/home/banner', 'slider' );
+
+      // Contact Page
       elseif(is_page(34)):
+        get_template_part( 'partials/contact/content');
+
+      // Bio Page
+      elseif(is_page(37)):
         get_template_part( 'partials/contact/content');
       else:
     ?>
       <section>
-        <h1><?php the_title(); ?></h1>
-        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <?php the_content(); ?>
-            <?php comments_template('', true); ?>
-          </article>
-        <?php endwhile; ?>
-        <?php else: ?>
-          <article>
-            <h2><?php _e('Sorry, nothing to display.', 'wp-blank'); ?></h2>
-          </article>
-        <?php endif; ?>
+        <div class="row">
+          <div class="col">
+            <h1><?php the_title(); ?></h1>
+            <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+              <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <?php the_content(); ?>
+                <?php comments_template('', true); ?>
+              </article>
+            <?php endwhile; ?>
+            <?php else: ?>
+              <article>
+                <h2><?php _e('Sorry, nothing to display.', 'wp-blank'); ?></h2>
+              </article>
+            <?php endif; ?>
+          </div>
+        </div>
       </section>
     <?php
       endif;
---
+++ wp-content/themes/twizzmatic/src/partials/bio/content.php
@@ -0,0 +1,29 @@
+<?php 
+	// Get ACF Fields
+	
+	$title = get_field('title');
+	$bio = get_field('bio');
+	$quote = get_field('quote');
+	$image = get_field('image');
+?>
+
+<section id="bio-content">
+	<div class="row">
+		<div class="col">
+			<div class="bio-image">
+				<img src="<?php echo $image; ?>" alt="TwizzMatic">
+			</div>
+			<div class="bio-quote">
+				<?php echo $quote; ?>
+			</div>
+		</div>
+		<div class="col">
+			<h1 class="bio-title yellow"><?php echo $title; ?></h1>
+			<div class="full-bio">
+				<div class="inner">
+					<?php echo $bio; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
