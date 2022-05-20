bd889cd2498b25694b2d847b12fdb959cc906920
Tel <tel@jsdfyodds.com>
2022-05-20 13:57:31 -0400

    move settings to top

--- wp-content/themes/twizzmatic/src/functions.php
+++ wp-content/themes/twizzmatic/src/functions.php
@@ -91,7 +91,8 @@ if (function_exists('acf_add_options_page')) {
 		'menu_title'	=> 'Theme Settings',
 		'menu_slug' 	=> 'theme-general-settings',
 		'capability'	=> 'edit_posts',
-		'redirect'		=> false
+		'redirect'		=> false,
+		'position' => 0
 	));
 
 	acf_add_options_sub_page(array(
